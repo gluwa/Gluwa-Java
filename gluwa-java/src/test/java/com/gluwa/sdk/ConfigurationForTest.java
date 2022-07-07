63537aafa6f3f134d275e026a8cdb0ca6e0cbab3
Timur Abasov <timur.abasov@gluwa.com>
2022-07-07 13:15:38 -0700

    Added logic for local and remote properties

--- gluwa-java/src/test/java/com/gluwa/sdk/ConfigurationForTest.java
+++ gluwa-java/src/test/java/com/gluwa/sdk/ConfigurationForTest.java
@@ -2,15 +2,20 @@ package com.gluwa.sdk;
 
 import org.web3j.crypto.Credentials;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConfigurationForTest extends Configuration {
 
-
     // Config variables
-    public static final String API_KEY = System.getenv("API_KEY_SANDBOX");
-    public static final String API_SECRET = System.getenv("API_SECRET_SANDBOX");
-    public static final String WEBHOOK_SECRET = System.getenv("WebhookSecret");
-    public static final String ETH_PUBLIC_ADDRESS = getEthAddress(System.getenv("SRC_PRIVATE_sNGNG_PROD"));
-    public static final String ETH_PRIVATE_KEY = System.getenv("SRC_PRIVATE_sNGNG_PROD");
+    public static final String API_KEY = getProperty("API_KEY_SANDBOX");
+    public static final String API_SECRET = getProperty("API_SECRET_SANDBOX");
+    public static final String WEBHOOK_SECRET = getProperty("WebhookSecret");
+    public static final String ETH_PUBLIC_ADDRESS = getEthAddress(getProperty("SRC_PRIVATE_sNGNG_PROD"));
+    public static final String ETH_PRIVATE_KEY = getProperty("SRC_PRIVATE_sNGNG_PROD");
 
     public ConfigurationForTest() {
         super.set__DEV__(true);
@@ -26,5 +31,57 @@ public class ConfigurationForTest extends Configuration {
         String publicAddress = cs.getAddress();
         return (publicAddress);
     }
+
+    private static String getProperty(String name){
+        String property = null;
+        try {
+            if (isRanLocally()){
+                property = getPropertyfromFile(name);
+            } else {
+                property = getPropertyfromEnv(name);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return property;
+    }
+
+    private static boolean isRanLocally(){
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win") ||  os.contains("mac")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String getPropertyfromEnv(String name){
+//        if (name.equals("SRC_PRIVATE_sNGNG_PROD")){
+//            return getEthAddress(System.getenv(name));
+//        } else {
+        return (System.getenv(name));
+    }
+
+    private static String getPropertyfromFile(String name){
+        String property;
+        Properties prop = new Properties();
+        try {
+            InputStream ip = new FileInputStream("C:\\GluwaDev\\Gluwa-Java\\gluwa-java\\src\\test\\resources\\properties\\config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        property = prop.getProperty(name);
+        return property;
+    }
+
+    public static void main (String[] args){
+        String os = System.getProperty("os.name").toLowerCase();
+        System.out.println(getEthAddress(getProperty("SRC_PRIVATE_sNGNG_PROD")));
+        System.out.println(os);
+    }
 }
+
 
